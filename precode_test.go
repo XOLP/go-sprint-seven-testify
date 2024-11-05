299fb1280c60812aa29b5a8dc60df99f0cff52af
Gleb Fedchun <gleb.fedchun@gmail.com>
2024-11-06 00:55:59 +0300

    second commit

--- precode_test.go
+++ precode_test.go
@@ -1,53 +1,13 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"strconv"
-	"strings"
-	"testing"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
@@ -63,7 +23,7 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
 
-	assert.Equal(t, len(list), totalCount)
+	assert.Len(t, list, totalCount)
 	// здесь нужно добавить необходимые проверки
 }
 
@@ -78,9 +38,7 @@ func TestMainHandlerAllOK(t *testing.T) {
 
 	require.Equal(t, http.StatusOK, status)
 
-	body := responseRecorder.Body.String()
-
-	assert.NotEmpty(t, body)
+	assert.NotEmpty(t, responseRecorder.Body)
 	// здесь нужно добавить необходимые проверки
 }
 
